1ddbbb4ffb50eaef9da0d32a2a9833307c287830
ferry5279 <2695506298@qq.com>
2020-07-22 23:23:16 +0800

    aaa

---
+++ src/pages/aaa/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '@@/Header'
+export default class index extends React.Component {
+  render() {
+    return <div>
+      <Header name = '购物车'/>
+      </div>;
+  }
+}
+
